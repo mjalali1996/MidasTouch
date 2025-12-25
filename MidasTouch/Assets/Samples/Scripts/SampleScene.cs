0103d8977bff55c4b01dc4faad13f14cbb28a145
MJ <mjalali1996.work@gmail.com>
2025-12-25 20:55:12 +0330

    Fixed Bazaar package

--- MidasTouch/Assets/Samples/Scripts/SampleScene.cs
+++ MidasTouch/Assets/Samples/Scripts/SampleScene.cs
@@ -1,9 +1,7 @@
 using MidasTouch.AD;
 using MidasTouch.Billing;
 using MidasTouch.Billing.Models;
-using UnityEditor.Purchasing;
 using UnityEngine;
-using UnityEngine.Purchasing;
 using UnityEngine.UI;
 
 namespace Samples.Scripts
